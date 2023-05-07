dd7057559f7aa9b226e5af91485a337df1f1c8ca
Holandsoest <holandsoest@gmail.com>
2023-05-07 15:10:16 +0200

    Add files for pip

---
+++ os-bedtime/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='os-bedtime',
+    version='0.0.0',
+    author='Koen van der Wolf',
+    author_email='holandsoest@gmail.com',
+    description='Put your computer to sleep, etc.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/Holandsoest/os_bedtime',
+    project_urls = {
+        "Bug Tracker": "https://github.com/Holandsoest/os_bedtime/issues"
+    },
+    license='MIT',
+    packages=['os-bedtime'],
+    install_requires=['psutil'],
+)
